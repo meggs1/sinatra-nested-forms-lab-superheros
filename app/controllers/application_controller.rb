98a7c354bfcd5c8d5b0f5aa0449502f87b73e3d2
meggs1 <megan.schertz@gmail.com>
2021-01-20 18:43:14 -0600

    commit tree 2c9e3161b864fe5787f131df9e76634f84611d8e

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,16 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        erb :super_hero
+    end
 
+    post '/teams' do
+        @team = Team.new(params[:team])
+        params[:team][:members].each do |a|
+            Hero.new(a)
+        end
+        @heroes = Hero.all
+        erb :team
+    end
 end
